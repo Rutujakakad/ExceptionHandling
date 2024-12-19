ed2c682ee74eee3bf95f58a6f622a43c5705dd8f
67418_Rutuja <rutubkakad@gmail.com>
2024-12-19 18:30:51 +0530

    Solved Exception Examples

---
+++ ExceptionHandling/ExceptionHandling/Generic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    //internal class Generic: Exception
+    //{
+
+    //    public static void StackPractise()
+    //    {
+    //        Stack<string> stk = new Stack<string>();
+    //        //stk.Push("cs.net");
+    //        //stk.Push("vb.net");
+    //        //stk.Push("asp.net");
+    //        //stk.Push("ado.net");
+    //        stk.Push(1);
+    //        foreach (string s in stk)
+    //        {
+    //            Console.WriteLine(s);
+    //        }
+    //    }
+
+
+
+
+
+    //    //********Queue*******
+    //    public static void QueuePractise()
+    //    {
+    //        Queue<string> que = new Queue<string>();
+    //        que.Enqueue("cs.net");
+    //        que.Enqueue("vb.net");
+    //        que.Enqueue("asp.net");
+    //        que.Enqueue("ado.net");
+
+    //        foreach (var element in que)
+    //        {
+    //            Console.WriteLine(element);
+    //        }
+    //    }
+
+    // }
+    //  ======================================================================>
+    class DivByZero : Exception
+    {
+        public DivByZero()
+        {
+            Console.Write("Exception occurred : ");
+        }
+
+        public double DivOperation(double num, double deno)
+        {
+            if (deno == 0)
+               // throw new DivByZero();
+                throw new NotImplementedException();
+            return num/ deno;
+        }
+    }                       
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{ //===============================Example (1)
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        try
+    //        {
+    //            Generic.StackPractise();
+    //            Console.WriteLine("Stack added");
+
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            //throw ex;
+    //            Console.WriteLine($"Error { ex.Message}");
+    //        }
+    //        finally
+    //        {
+    //            Console.WriteLine("Code ends...");
+    //        }
+    //    }
+    //}
+
+    //================================>Example(2)
+    //class Array
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] arr = { 1, 2, 3, 8, 6, 12, 22 };
+
+    //        //for (int i = 0; i < arr.Length; i++)
+    //        //{
+    //        //    Console.WriteLine(arr[i]);
+    //        //}
+
+    //        try
+    //        {
+    //            Console.WriteLine(arr[8]);
+    //        }
+    //        catch (Exception ex) 
+    //        {
+    //             Console.WriteLine(ex);
+    //            Console.WriteLine("An Exception has occurred : {0}", ex.Message);
+    //        }
+    //    }
+    //}
+
+    //============================================>Example(3)
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                DivByZero obj = new DivByZero();
+            double num = 9, deno = 0, quotient;
+            try
+            {
+                // Code block that may cause an exception
+                Console.WriteLine("Quotient = {0}", (float)obj.DivOperation(num, deno));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //private double DivOperation(double num, double den)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
+
+    
+        
+    
